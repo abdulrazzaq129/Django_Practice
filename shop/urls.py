f812d939826b22f7cfa335598a4727cf8ad718b5
Abdul Razzaq <itsabdulrazzaq181@email.com>
2026-04-15 17:47:17 +0500

    add register and login authentication

--- shop/urls.py
+++ shop/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('article_detail/<int:year>/<int:month>/<int:day>/', views.article_detail, name='article_detail'),
     path('', views.home, name='home'),
     re_path(r'^article/(?P<year>[0-9]{4})/$', views.article, name='article'),
-    path('student/', views.student_list, name='student_list '),
-    path('student_edit/<int:id>/', views.student_edit, name='student_edit '),
-
-
+    path('student/', views.student_list, name='student_list'),
+    path('student_edit/<int:id>/', views.student_edit, name='student_edit'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
